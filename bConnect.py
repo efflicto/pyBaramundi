8b4dfece19f6cbd80783c1d2903a560ec5bf2cbf
efflicto <sven.soost@gmail.com>
2015-01-28 14:02:38 +0100

    Addet more functionality

--- bConnect.py
+++ bConnect.py
@@ -1,3 +1,4 @@
+import re
 import urllib2
 import json
 import base64
@@ -65,8 +66,75 @@ class BConnect:
 
         return self.connect(self.bms_search_url+'?type=job&term=%s' % keyword)
 
+    def get_jobs_with_error(self):
+
+        #to define this you need to find the error messages in your language.
+        #see get_unique_status_text() method to find out whats your StateText to match
+        ERR_DEF = [
+            'Fehler',
+            'keine Verbindung',
+            'rungstimeout',
+            'fehlgeschlagen',
+            'keine ausf',
+            'nicht gestartet',
+            'schlugen fehl',
+        ]
+
+        data = self.connect(self.bms_job_instances_url)
+        jobs_with_err=[]
+
+        for n in data:
+            for x in ERR_DEF:
+                if x in n['StateText']:
+                    jobs_with_err.append(n)
+
+        return json.dumps(jobs_with_err)
+
+    def get_job_error_count(self):
+
+        return len(json.loads(self.get_jobs_with_error()))
 
 
+    def get_jobs(self):
+
+        return self.connect(self.bms_job_instances_url)
+
+    def get_jobs_by_state(self,state_id):
+        #See bConnect.pdf from baramundi documentation:
+        # [BmsNetState]
+        # -1 Unknown
+        # 0 Assigned
+        # 1 Running
+        # 2 FinishedSuccess
+        # 3 FinishedError
+        # 4 FinishedCanceled
+        # 5 ReScheduled
+        # 6 ReScheduledError
+        # 7 WaitingForUser
+        # 8 RequirementsNotMet
+        # 9 Downloading
+
+        data = self.connect(self.bms_job_instances_url)
+        jobs_running =[]
+
+        for n in data:
+            if int(n['BmsNetState']) ==state_id:
+                jobs_running.append(n)
+
+        return json.dumps(jobs_running)
+
+    def get_jobs_running_count(self):
+
+        return len(json.loads(self.get_jobs_by_state(1)))
+
+
+    def get_jobs_scheduled(self):
+
+        return self.get_jobs_by_state(5)
+
+    def get_jobs_scheduled_count(self):
+
+        return len(json.loads(self.get_jobs_scheduled()))
 
 
     ###Client methods
@@ -104,14 +172,18 @@ class BConnect:
 
     def get_client_jobs(self, id):
 
-        data = self.connect(self.bms_job_instances_url)
-        x = []
+        #old version
+        # data = self.connect(self.bms_job_instances_url)
+        # x = []
+        #
+        # for n in data:
+        #     if id in n['EndpointId']:
+        #         x.append(n)
+        #
+        # return json.dumps(x)
 
-        for n in data:
-            if id in n['EndpointId']:
-                x.append(n)
-
-        return json.dumps(x)
+        #new in 2014 R2
+        return self.connect(self.bms_job_instances_url+'?endpointid=%s'%id)
 
     def get_client_count(self):
 
@@ -139,7 +211,7 @@ class BConnect:
             if n['Id'] not in inactive_client_ids:
                 active_clients.append(n)
 
-        return active_clients
+        return json.dumps(active_clients)
 
     def get_clients_inactive(self):
 
@@ -153,6 +225,27 @@ class BConnect:
     def get_clients_from_keyword(self,keyword): #You can search mostly everything with this method
 
             return self.connect(self.bms_search_url+'?type=endpoint&term=%s' %keyword)
+
+
+
+
+    #Help methods
+
+    def get_unique_status_text(self):
+        re1 = '((?:(?:[0-2]?\\d{1})|(?:[3][01]{1}))[-:\\/.](?:[0]?[1-9]|[1][012])[-:\\/.](?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d])'	# DDMMYYYY 1
+        re2 = '.*?'	# Non-greedy match on filler
+        re3 = '((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])?(?:\\s?(?:am|AM|pm|PM))?)'	# HourMinuteSec 1
+        re4 = '.*?'	# Non-greedy match on filler
+        re5 = '((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?![\\d])'	# IPv4 IP Address 1
+        rg = re.compile(re1+re2+re3+re4,re.IGNORECASE|re.DOTALL)
+
+
+        x=[]
+        for n in self.get_jobs():
+            if re.sub(re5,'',re.sub(rg,'',n['StateText'])) not in x:
+                x.append(re.sub(re5,'',re.sub(rg,'',n['StateText'])))
+
+        return x
 
 
 
